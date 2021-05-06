fff693c1cfa8109ca0d96931d3b68fe5a159e52e
Elizabeth Alcalá <elizabethalcala28@gmail.com>
2021-05-05 22:08:48 -0500

    chore: config prettier and eslint

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,10 @@
-import Head from 'next/head'
-import Image from 'next/image'
+import Head from 'next/head';
+import Image from 'next/image';
 
 export default function Home() {
   return (
-    <div >
-      new image
+    <div>
+      <span>sadnew image</span>
     </div>
-  )
+  );
 }
